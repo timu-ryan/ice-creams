f997da28ec1e6c5f66466775875a95cfbd500402
Timur Gafarov <timu.ryan@yandex.ru>
2024-10-24 01:39:31 +0300

    search added

--- src/components/IceCreamList/IceCreamList.js
+++ src/components/IceCreamList/IceCreamList.js
@@ -5,13 +5,19 @@ import IceCreamItem from '../IceCreamItem/IceCreamItem'
 const IceCreamList = ({list, isSavedPage, setPrice, setNumber}) => {
 
   const [visibleNumber, setVisibleNumber] = useState(10);
+  const [inputValue, setInputValue] = useState('');
+  function handleInputChange(e) {
+    setInputValue(e.target.value)
+  }
 
   function addMoreClick() {
     setVisibleNumber(visibleNumber => visibleNumber+=2)
   }
-
+  
   return (
     <>
+      {!isSavedPage && <label for="icecream" className='input-label'>введите название:</label>}
+      {!isSavedPage && <input id='icecream' type='text' value={inputValue} onChange={handleInputChange} className='input'/>}
       <ul className='icecream-list'>
         {list.filter((item) => {
           // если сохраненная страница и если есть в localStorage
@@ -19,8 +25,12 @@ const IceCreamList = ({list, isSavedPage, setPrice, setNumber}) => {
             return false;
           }
           return true;
+        }).filter(item => {
+          if (isSavedPage) return true; // нет поиска на странице сохраненных
+          return item.title.toLowerCase().includes(inputValue.toLowerCase())
         }).filter((item, i) => {
-          if(!isSavedPage && i + 1 > visibleNumber) return false;
+          // отображаются все, если идет поиск и если это страница сохраненных
+          if(!isSavedPage && inputValue === '' && i + 1 > visibleNumber) return false;
           return true;
         }).map(item => {
           const {id, title, image, cost} = item;
@@ -35,7 +45,7 @@ const IceCreamList = ({list, isSavedPage, setPrice, setNumber}) => {
             />)
         })} 
       </ul>
-      {(visibleNumber < list.length && !isSavedPage) && <button onClick={addMoreClick} className='show-more-btn'>показать еще</button>}
+      {(visibleNumber < list.length && !isSavedPage && inputValue === '') && <button onClick={addMoreClick} className='show-more-btn'>показать еще</button>}
     </>
   )
 }
