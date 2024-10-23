c238bc4f4b1cda757e4ec92b972be46341804183
Timur Gafarov <timu.ryan@yandex.ru>
2024-10-23 19:11:27 +0300

    add more button added

--- src/components/IceCreamList/IceCreamList.js
+++ src/components/IceCreamList/IceCreamList.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './IceCreamList.css'
 import IceCreamItem from '../IceCreamItem/IceCreamItem'
 
 const IceCreamList = ({list, isSavedPage}) => {
 
+  const [visibleNumber, setVisibleNumber] = useState(10);
+
+  function addMoreClick() {
+    setVisibleNumber(visibleNumber => visibleNumber+=2)
+  }
+
   return (
-    <ul className='icecream-list'>
-      {list.filter((item) => {
-        if(isSavedPage && !localStorage.getItem(item.id)) {
-          return false;
-        }
-        return true;
-      }).map(item => {
-        const {id, title, image, cost} = item;
-        const iceCreamItem = {id, title, image, cost};
-        return (
-          <IceCreamItem 
-            key={id}
-            iceCreamItem={iceCreamItem}
-            isSavedPage={isSavedPage}
-          />)
-      })} 
-    </ul>
+    <>
+      <ul className='icecream-list'>
+        {list.filter((item) => {
+          // если сохраненная страница и если есть в localStorage
+          if(isSavedPage && !localStorage.getItem(item.id)) {
+            return false;
+          }
+          return true;
+        }).filter((item, i) => {
+          if(!isSavedPage && i + 1 > visibleNumber) return false;
+          return true;
+        }).map(item => {
+          const {id, title, image, cost} = item;
+          const iceCreamItem = {id, title, image, cost};
+          return (
+            <IceCreamItem 
+              key={id}
+              iceCreamItem={iceCreamItem}
+              isSavedPage={isSavedPage}
+            />)
+        })} 
+      </ul>
+      {(visibleNumber < list.length) && <button onClick={addMoreClick} className='show-more-btn'>показать еще</button>}
+    </>
   )
 }
 
