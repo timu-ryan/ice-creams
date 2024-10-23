795ad8d5c7ae2f48e0d5f79dd529dbb83e328266
Timur Gafarov <timu.ryan@yandex.ru>
2024-10-24 01:19:39 +0300

    class changed to className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <h3 className='footer__title'>Гафаров Тимур</h3>
-      <address class="footer__address">
-        <a href="https://t.me/timuryanst" rel="noreferrer" target="_blank" class="footer__link">telegram</a>
-        <a href="tel:+79872729464" rel="noreferrer" target="_blank" class="footer__link">+7 987 272 9464</a>
-        <a href="mailto:timu.ryanst@gmail.com" rel="noreferrer" target="_blank" class="footer__link">timu.ryanst@gmail.com</a>
+      <address className="footer__address">
+        <a href="https://t.me/timuryanst" rel="noreferrer" target="_blank" className="footer__link">telegram</a>
+        <a href="tel:+79872729464" rel="noreferrer" target="_blank" className="footer__link">+7 987 272 9464</a>
+        <a href="mailto:timu.ryanst@gmail.com" rel="noreferrer" target="_blank" className="footer__link">timu.ryanst@gmail.com</a>
       </address>
     </footer>
   )
